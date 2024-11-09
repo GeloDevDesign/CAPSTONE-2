cc0fd42479c8cedcda24885c262c7c87984cf9a8
GeloDevDesign <abserenuela@ccc.edu.ph>
2024-11-09 18:18:50 +0800

    tangina

--- frontend/src/components/posSidebar.vue
+++ frontend/src/components/posSidebar.vue
@@ -241,10 +241,7 @@ onMounted( ()=>{
     <div>
       <div class="avatar flex flex-col gap-2 items-center">
         <div class="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
-          <img :src="userData.gender === 'Male' ? '/frontend/dist/maleicon.jpg' : '/frontend/dist/girlicon.jpg'" />
-         
-         
-         
+          <img :src="userData.gender === 'Male' ? '/frontend/public/maleicon.jpg' : '/frontend/public/girlicon.jpg'" />  
         </div>
         <p class="text-white text-sm">{{ userData.name }}</p>
       </div>
